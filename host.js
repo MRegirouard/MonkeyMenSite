4545f11583c45b9b3720b7c66d37da33c7db0a66
Ethan Girouard <mr.egirouard@gmail.com>
2021-07-29 14:55:56 -0400

    Write http server

---
+++ host.js
@@ -0,0 +1,31 @@
+const http = require('http')
+const fs = require('fs')
+
+// Run with "PORT=8081 node host.js" to use a port other than 80
+const port = process.env.PORT || 80
+const indexFile = 'index.html'
+
+ // Read the main page file
+fs.readFile(indexFile, (err, data) =>
+{
+    if (err)
+    {
+        console.error('[ FAIL ] Error reading', indexFile, ':', err)
+        process.exit(1)
+    }
+
+    // Respond to requests
+    const server = http.createServer((req, res) =>
+    {
+        console.debug('[  OK  ] Received connection. URL:', req.url)
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(data)
+    })
+
+    // Listen for requests
+    server.listen(port, () =>
+    {
+        console.info('[  OK  ] Server is running on port', port)
+    })
+})
